a5e510476ab57f9962a76047bf1bbfb5e8afcb20
Yadu Krishnan <mailmeyadukrishnanm3@gmail.com>
2025-07-14 07:11:55 +0530

    added port

--- main.py
+++ main.py
@@ -55,5 +55,7 @@ def convert_pdf():
 
     return jsonify({'error': 'File processing failed'}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
